76ca14c32749fc6d0066f5349a7e1d2b8c51e7b7
Brandon Fong <fong.m.brandon97@gmail.com>
2025-06-13 08:46:58 -0700

    Adding numbers to the neo-tree sidebar

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,10 +15,35 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        hide_dotfiles = false, -- Show hidden files
+        hide_gitignored = false, -- Still hide gitignored files
+        -- visible = true,       -- Or set this to true if you want them dimmed rather than fully hidden
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+    },
+    -- ADD THIS 'event_handlers' BLOCK:
+    event_handlers = {
+      {
+        event = 'neo_tree_buffer_enter',
+        handler = function()
+          -- Set absolute line numbers
+          vim.cmd 'setlocal number'
+          -- OR set relative line numbers
+          -- vim.cmd("setlocal relativenumber")
+        end,
+      },
+      {
+        event = 'neo_tree_buffer_leave',
+        handler = function()
+          -- Optionally, unset line numbers when leaving the Neo-tree buffer
+          vim.cmd 'setlocal nonumber'
+          vim.cmd 'setlocal norelativenumber'
+        end,
       },
     },
   },
